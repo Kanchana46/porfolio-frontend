441aaeeb513a522364444a6c64e68e92a8c0cfba
Kanchana46 <kanchana.rdg@gmail.com>
2022-10-06 13:18:01 +0530

    .env changes

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'y1ekj92b',
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: 'skADmFIyO1lhVRUB1arL9spLXvnKusmR0sCGOk3hIRNYG6ebg6VQuFG9sja0uflfr3BqIFw2qLbw1Mkhei177nhwKgPUly7RZPeWXjtwmBVJlhZv30bGjGAQbStOr2TCDSPjz8zopx5MJ8IU7eIIb0jywPbcLHWbcLEXkNdZyIExRcLJWpAs'
+    token: process.env.REACT_APP_SANITY_TOKEN
 });
 
 const builder = imageUrlBuilder(client);
